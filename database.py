7d676f1a6a3892fecf35bb8d67e4ffb7e5d2000a
michaelmoju <michaelmoju@gmail.com>
2023-04-22 15:13:12 -0400

    build database.py

---
+++ database.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+class ResDB:
+    def __init__(self) -> None:
+        pass
+
+    def connect():
+        return
+    
+    def get_rests_by_city(city_name):
+        rests = []
+        return rests
+    
+
+class UserDB:
+    def __init__(self) -> None:
+        pass
+
+    def connect():
+        return
+    
+    def get_user(user_id):
+        return
+    
+    
+class RecordDB:
+    def __init__(self) -> None:
+        pass
+
+    def connect():
+        return
+    
+    def get_record_by_user(user_id):
+        records = []
+        return records
